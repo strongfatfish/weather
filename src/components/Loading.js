a549f9e7657a957542f9e5a04d3a6efbcb24d8d7
strongfatfish <strongfatfish@gmail.com>
2020-04-06 15:31:17 +0800

    创建Loading

---
+++ src/components/Loading.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import "./../style/loading.css"
+function Loading() {
+    return (
+        <div className="loading">
+            <div className="transition"/>
+        </div>
+    );
+}
+
+export default Loading;
